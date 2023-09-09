dee4a3ece170e2263f64544269b271a4d55316c5
Oscar <a20193315@pucp.edu.pe>
2023-09-08 20:40:59 -0500

    first commit

---
+++ lab4_20193315/src/main/java/com/example/lab4_20193315/Lab420193315Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20193315;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420193315Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420193315Application.class, args);
+    }
+
+}
---
+++ lab4_20193315/src/test/java/com/example/lab4_20193315/Lab420193315ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20193315;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420193315ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
